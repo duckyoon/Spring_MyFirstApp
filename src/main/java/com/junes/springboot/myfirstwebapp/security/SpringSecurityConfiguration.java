91563097d33b844a2d6bfd5102fdcb2202a8638e
duckyoon <dkdlel616@gmail.com>
2025-07-20 15:13:36 +0900

    feat: H2 데이터 베이스 사용을 위한 Spring Security 설정

--- src/main/java/com/junes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/junes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -2,13 +2,17 @@ package com.junes.springboot.myfirstwebapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -45,4 +49,18 @@ public class SpringSecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
+    // Spring Security 6.x 버전 이후에는 메서드 체이닝 방식 대신 람다 기반의 설정 방식을 사용
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated())
+                // 기본 로그인 페이지 활성화
+            .formLogin(withDefaults()) // 기본 로그인 페이지 활성화
+                //CSRF 보호 비활성화 (개발 환겨에서만 사용)
+            .csrf(csrf -> csrf.disable())
+                // iframe 허용
+            .headers(headers -> headers.frameOptions(fram -> fram.disable()));
+
+        return http.build();
+    }
 }
